f356c362e77c8e9a7bae636e967d8949f290096f
hxwang <hxwang@asu.edu>
2020-05-31 22:44:18 -0700

    d3 example

--- bindings/jupyter-modules/jupyter-notebook-search/src/index.js
+++ bindings/jupyter-modules/jupyter-notebook-search/src/index.js
@@ -1,3 +1,26 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import * as d3 from 'd3';
+export default props => {
+  const svgRef = useRef(null);
+  const {data} = props;
+  const cells = (data && data.notebooks) || [];
+  useEffect(() => {
+    const sel = d3
+      .select(svgRef.current)
+      .selectAll('text')
+      .data(cells, d => d);
 
-export default props => <div>{`Notebook search ... `}</div>;
+    const enter = sel.enter().append('text');
+    sel.exit().remove();
+
+    sel
+      .merge(enter)
+      .text(d => d)
+      .attr('y', (d, i) => (i + 1) * 20);
+  });
+  return (
+    <div>
+      <svg ref={svgRef} width="1000px" height="600px" />
+    </div>
+  );
+};
